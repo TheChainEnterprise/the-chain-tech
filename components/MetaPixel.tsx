b4cf5bc291617819d74338178a94a1d9ccfcec7d
Andres Karhu <mrandreskarhu@gmail.com>
2026-07-13 16:06:46 +0700

    Replace Meta Pixel with official Meta implementation

--- components/MetaPixel.tsx
+++ components/MetaPixel.tsx
@@ -1,35 +1,61 @@
 "use client";
 
+import Script from "next/script";
 import { useEffect } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 const PIXEL_ID = "872100682225985";
 
+declare global {
+  interface Window {
+    fbq: (...args: any[]) => void;
+  }
+}
+
 export default function MetaPixel() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
-    async function initPixel() {
-      const ReactPixel = (await import("react-facebook-pixel")).default;
-
-      ReactPixel.init(PIXEL_ID);
-
-      ReactPixel.pageView();
+    if (typeof window.fbq === "function") {
+      window.fbq("track", "PageView");
     }
+  }, [pathname]);
 
-    initPixel();
-  }, []);
+  return (
+    <>
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {
+            if(f.fbq)return;
+            n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n;
+            n.loaded=true;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=true;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s);
+          }(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
 
-  useEffect(() => {
-    async function trackPage() {
-      const ReactPixel = (await import("react-facebook-pixel")).default;
+          fbq('init', '${PIXEL_ID}');
+          fbq('track', 'PageView');
+        `}
+      </Script>
 
-      ReactPixel.pageView();
-    }
-
-    trackPage();
-  }, [pathname, searchParams]);
-
-  return null;
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          alt=""
+          src={`https://www.facebook.com/tr?id=${PIXEL_ID}&ev=PageView&noscript=1`}
+        />
+      </noscript>
+    </>
+  );
 }
